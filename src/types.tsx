23c0ddac9d113134ba82add47909b995fda0d9a5
igune <igunejiro@gmail.com>
2024-08-11 16:40:03 -0700

    shadcn components and broken UI

---
+++ src/types.tsx
@@ -0,0 +1,14 @@
+export type projects = {
+  name: string;
+  stack: string[];
+  github: string;
+  description: string;
+  image?: string;
+  video?: string;
+};
+
+export type experience = {
+  jobTitle: string;
+  yoe: number;
+  description: string;
+};
