7de869f7736a3909ea2d7d683aa05c38675e2e2e
igune <igunejiro@gmail.com>
2024-08-11 16:06:43 -0700

    shadcn install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require("tailwindcss/defaultTheme");
-
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
   theme: {
-    screens: {
-      xs: { min: "300px", max: "639px" },
-      ...defaultTheme.screens,
-      // 'sm': {'min': '640px', 'max': '767px'},
-
-      // 'md': {'min': '768px', 'max': '1023px'},
-
-      // 'lg': {'min': '1024px', 'max': '1279px'},
-
-      // 'xl': {'min': '1280px', 'max': '1535px'},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
